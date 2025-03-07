3e796cf5d950b200d9e2015a69db5e1a6fed2187
unLog12345 <Logun.Williston@GCTCOK.EDU>
2025-03-07 14:13:33 -0600

    Initial commit

---
+++ Casting_Williston_Logun/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Casting_Williston_Logun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks user to enter a whole number and assigns input to integer
+            Console.WriteLine("Please enter a whole number!");
+            int myFavNum = Convert.ToInt32(Console.ReadLine());
+
+            // Prints favorite number to console
+            Console.WriteLine("Your favoritenumber is " + myFavNum);
+
+            // Asks user a question and assigns response to boolean
+            Console.WriteLine("Do you like video games? (Please enter 'true' or 'false')");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+            // Print the result of question asked
+            Console.WriteLine($"It is {isTrue} that I like video games!");
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Casting_Williston_Logun
+ 
